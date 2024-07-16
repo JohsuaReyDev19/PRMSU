99346a67c63cc891ed206cb84d017d44769af677
Johsua Burce <burcejosh19@gmail.com>
2024-07-16 21:21:32 +0800

    first commit

---
+++ PRMSU_WEB/src/components/enroll/Enroll.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Enroll.css'
+
+const Enroll = () => {
+  return (
+    <div className='img-cont enroll'>
+        <h1>ENROLL NOW</h1>
+        <p>click images to full view</p>
+        <br />
+        <div className='images'>
+            <a href="https://scontent.fmnl7-1.fna.fbcdn.net/v/t39.30808-6/448019791_896375428961673_6837764652845828673_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeE16_7Odl_hpUvcuPoBRI9QubcEPz6fBq65twQ_Pp8GrgwcNcWXQ94pfnSYyTZfVQ_cCH6t4lJDbtdYVbOPyq2O&_nc_ohc=mCJcu7zgOV8Q7kNvgETu2zH&_nc_zt=23&_nc_ht=scontent.fmnl7-1.fna&oh=00_AYD78HMmdM23ijqpLUtrW5hdSyKJVoaZKHsWWdRyF5aZDg&oe=669C2C81">
+            <img src="https://scontent.fmnl7-1.fna.fbcdn.net/v/t39.30808-6/448019791_896375428961673_6837764652845828673_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeE16_7Odl_hpUvcuPoBRI9QubcEPz6fBq65twQ_Pp8GrgwcNcWXQ94pfnSYyTZfVQ_cCH6t4lJDbtdYVbOPyq2O&_nc_ohc=mCJcu7zgOV8Q7kNvgETu2zH&_nc_zt=23&_nc_ht=scontent.fmnl7-1.fna&oh=00_AYD78HMmdM23ijqpLUtrW5hdSyKJVoaZKHsWWdRyF5aZDg&oe=669C2C81" alt="" />
+            </a>
+            <a href="https://scontent.fmnl7-1.fna.fbcdn.net/v/t39.30808-6/448019238_896349548964261_8293450988467695916_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeFGopPfIF1uQGWsjYfOwKxhtx2o_7MnOpa3Haj_syc6ljxC8O1gcPUnoJTkA844-vBktKt9-WAbKYjWkysHvA-3&_nc_ohc=3EVCFs46I94Q7kNvgFMGk0n&_nc_zt=23&_nc_ht=scontent.fmnl7-1.fna&oh=00_AYBOEtzgOLPsIYSt2zndthMG7OESG7ecxZQEFdgkGXlY7Q&oe=669C4123">
+            <img src="https://scontent.fmnl7-1.fna.fbcdn.net/v/t39.30808-6/448019238_896349548964261_8293450988467695916_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeFGopPfIF1uQGWsjYfOwKxhtx2o_7MnOpa3Haj_syc6ljxC8O1gcPUnoJTkA844-vBktKt9-WAbKYjWkysHvA-3&_nc_ohc=3EVCFs46I94Q7kNvgFMGk0n&_nc_zt=23&_nc_ht=scontent.fmnl7-1.fna&oh=00_AYBOEtzgOLPsIYSt2zndthMG7OESG7ecxZQEFdgkGXlY7Q&oe=669C4123" alt="" />
+            </a>
+        </div>
+        <a href="https://icloudph.com/prmsu-candelaria/sias/ " className='admission-btn'>Go to Admission</a>
+    </div>
+  )
+}
+
+export default Enroll
